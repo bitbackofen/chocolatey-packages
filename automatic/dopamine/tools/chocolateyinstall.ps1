e7aca8c94862aedc022eb4e1d7528009aee6700c
Matthieu Harlé <harle.matthieu@gmail.com>
2017-07-29 22:10:41 +0200

    Cleaned up files

--- automatic/dopamine/tools/chocolateyinstall.ps1
+++ automatic/dopamine/tools/chocolateyinstall.ps1
@@ -10,18 +10,14 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url           = $url # download url, HTTPS preferred
-  #url64bit      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
-  #file         = $fileLocation
+  url           = $url
 
-  softwareName  = 'dopamine*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'dopamine*' 
 
   checksum      = $checksum
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  #checksum64    = ''
-  #checksumType64= 'sha256'
+  checksumType  = 'sha256' 
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
   validExitCodes= @(0, 3010, 1641)
 }
 
