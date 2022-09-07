9a05fc3cf67a177eeb3858464d7c80e30af07409
elpatron68 <elpatron@cepheus.uberspace.de>
2022-09-07 12:57:33 +0200

    Dopamine v2.0.9

--- automatic/dopamine/tools/chocolateyinstall.ps1
+++ automatic/dopamine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 
 $packageName= 'dopamine'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://www.digimezzo.com/content/software/dopamine/Dopamine%202.0.3%20(Release).msi'
-$checksum = '60b7cfaf96ad54d1046cbbd9239ae45c'
+$url = 'https://www.digimezzo.com/content/software/dopamine/Dopamine%202.0.9%20(Release).msi'
+$checksum = '6009410b99cc48b15c62b748c7e61d8586cf3ffbb4d2ea03d442e3557f272ffe'
 
 $packageArgs = @{
   packageName   = $packageName
